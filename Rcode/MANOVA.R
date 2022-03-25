868b691d5f86fee6ba2e48f64ca0f78114b9f363
Ayato Tanemura <1998ayatotaemura@gmail.com>
2022-03-25 23:38:46 +1300

    add visualization

--- Rcode/MANOVA.R
+++ Rcode/MANOVA.R
@@ -33,3 +33,40 @@ qqnorm(x$x6)
 qqline(x$x6)
 hist(x$x6)
 plot(density(x$x6))
+
+##https://stats.stackexchange.com/questions/101274/how-to-interpret-a-qq-plot
+qqnorm(x$x7)
+qqline(x$x7)
+hist(x$x7)
+plot(density(x$x7))
+
+#Outliers detection and removal
+
+outliersx6<-boxplot(x6 ~ x1, data = x,
+                    xlab = "customer type", ylab = "product quality",
+                    main = "compare", col = c("#00AFBB", "#E7B800", "#FC4E07"),
+                    names=c("<1 year","1-5 years",">5 years") )$out
+print(outliersx6)
+
+out1<-x[x$x1==3 & x$x6==7.6,]
+print(out1)
+out2<-x[x$x1==1 & x$x6==9.5,]
+print(out2)
+
+outliersx7<-boxplot(x7 ~ x1, data = x,
+                    xlab = "customer type", ylab = "E-Commerce",
+                    main = "compare", col = c("#00AFBB", "#E7B800", "#FC4E07")
+                    , names=c("<1 year","1-5 years",">5 years") )$out
+print(outliersx7)
+
+out3<-x[x$x1==2 & (x$x7==5.1 | x$x7==5.5),]
+print(out3)
+out4<-x[x$x1==1 & x$x7==5.6,]
+print(out4)
+
+outAll<- rbind(out1,out2, out3, out4)
+print(outAll)
+# 10 observations will be deleted from the dataset
+
+x1<-x[-which(x$id %in% outAll$id),]
+
